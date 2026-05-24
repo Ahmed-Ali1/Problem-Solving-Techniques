de14eddd65b74d608b2189022f2c3ccb698cf792
Ahmed-Ali1 <ahmed.good.man007@gmail.com>
2026-05-24 12:41:55 +0300

    updating string, maze, generator, utilites

--- Brute_Force/Brute_Force/Generator.cs
+++ Brute_Force/Brute_Force/Generator.cs
@@ -8,6 +8,8 @@ namespace Brute_Force
     {
         private static readonly Random _random = new Random();
 
+        // fixed size maze for full size windows terminal
+        // fixed start points , end points
         public static int[,] GenerateMaze(int rows=60, int cols=104, int sRow=1, int sCol =1)
         {
             int eRow = rows - 2;
@@ -24,28 +26,23 @@ namespace Brute_Force
                     }
                     else
                     {
-                        // جدران داخلية بنسبة 35%
                         grid[r, c] = (_random.Next(0, 100) < 35) ? 1 : 0;
                     }
                 }
             }
 
-            // الخطوة 2: حفر مسار متعرج وعشوائي (Random Walk)
             int currR = sRow;
             int currC = sCol;
 
-            // استمرار الحفر طالما لم نصل للهدف بعد
             while (currR != eRow || currC != eCol)
             {
-                grid[currR, currC] = 0; // فتح الخلية الحالية
+                grid[currR, currC] = 0; 
 
-                // تحديد الخيارات المتاحة للتقرب من الهدف
                 bool canMoveRow = currR != eRow;
                 bool canMoveCol = currC != eCol;
 
                 if (canMoveRow && canMoveCol)
                 {
-                    // اختيار عشوائي 50% للتحرك إما رأسياً أو أفقياً
                     if (_random.Next(0, 2) == 0)
                     {
                         currR += (eRow > currR) ? 1 : -1;
@@ -57,17 +54,14 @@ namespace Brute_Force
                 }
                 else if (canMoveRow)
                 {
-                    // مجبر على التحرك رأسياً فقط
                     currR += (eRow > currR) ? 1 : -1;
                 }
                 else if (canMoveCol)
                 {
-                    // مجبر على التحرك أفقياً فقط
                     currC += (eCol > currC) ? 1 : -1;
                 }
             }
 
-            // الخطوة 3: تأكيد فتح البداية والنهاية
             grid[sRow, sCol] = 0;
             grid[eRow, eCol] = 0;
 
